dccafd286a55d4d5ade57bffe47663c14024223c
iamsaif <saifulhasan580@gmail.com>
2021-03-13 02:50:52 +0530

    started

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express=require('express');
 const app =express();
 const path=require('path')
 const bodyParser=require('body-parser')
+const methodOverride=require('method-override')
 
+app.use(methodOverride("_method"));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.urlencoded({extended:true}));
 app.set('views',path.join(__dirname,'views'));
@@ -12,22 +14,26 @@ let friends=[
     {
         id:1,
         name:"saif",
-        age:24
+        age:24,
+        location:"Banglore"
     },
     {
         id:2,
         name:"irfan",
-        age:24
+        age:24,
+        location:"Lucknow"
     },
     {
         id:3,
         name:"anand",
-        age:24
+        age:24,
+        location:"Sitapur"
     },
     {
         id:4,
         name:"vikas",
-        age:24
+        age:24,
+        location:"Gorakhpur"
     },
 
 ]
@@ -48,6 +54,34 @@ app.get('/friends/new',(req,res)=>{
 app.post('/friends',(req,res)=>{
     const {name,age}=req.body;
     friends.push({id:5,name,age});
+    res.redirect('/friends')
+})
+
+
+app.get('/friends/:id',(req,res)=>{
+    const id=req.params.id;
+    const friendsInfo=friends.find(f=>f.id===parseInt(id));
+    res.render('show.ejs',{friendsInfo})
+})
+
+app.get('/friends/:id/edit',(req,res)=>{
+    const {id}=req.params;
+    const friendsInfo=friends.find(f=>f.id===parseInt(id));
+    res.render('edit.ejs',{friendsInfo})
+})
+app.patch('/friends/:id',(req,res)=>{
+    const editFriend=req.body;
+    const {id}=req.params;
+    let friendsInfo=friends.find(f=>f.id===parseInt(id));
+    friendsInfo.name=editFriend.name;
+    friendsInfo.age=editFriend.age;
+    friendsInfo.location=editFriend.location;
+    res.redirect('/friends');
+})
+
+app.delete('/friends/:id',(req,res)=>{
+    const {id}=req.params;
+    friends=friends.filter(i=>i.id!=id);
     res.redirect('/friends')
 })
 
