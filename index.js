9bc39aae9077a4c11c9059b54905deeecaba9269
iamsaif <saifulhasan580@gmail.com>
2021-03-13 00:46:19 +0530

    started

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express=require('express');
 const app =express();
 
+app.get('/',(req,res)=>{
+    res.send("REstuful routes and crud operation without database");
+})
 
 
 app.listen(8000,()=>{
